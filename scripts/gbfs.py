Fix: degress to radians

scripts/gbfs.py
@@ -13,7 +13,7 @@ ms_to_sec = lambda x: x * (10**-3)
 ms_to_ns = lambda x: x * (10**6)
 s_to_ns = lambda x: x * (10**9)
 
-INTERVAL_IN_MS = 5
+INTERVAL_IN_MS = 1000
 INTERVAL_IN_S = ms_to_sec(INTERVAL_IN_MS)
 INTERVAL_IN_NS = ms_to_ns(INTERVAL_IN_MS)
 
@@ -45,7 +45,9 @@ def odom_callback(odom):
     global tr
     global r2
     global godom
+    global gscan
     global read_first_odom
+    global fs
 
     t2 = int(str(odom.header.stamp)) 
     
@@ -58,17 +60,26 @@ def odom_callback(odom):
         pt2[1] = odom.pose.pose.position.y
         orientation_q = odom.pose.pose.orientation
 
-        orientation_list = [orientation_q.w, orientation_q.x, orientation_q.y, orientation_q.z]
-        (theta, _, _) = euler_from_quaternion(orientation_list)
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (_, _, theta) = euler_from_quaternion(orientation_list)
         pt2[2] = theta
 
         if read_first_odom:
             tr = np.sqrt((pt1[0]-pt2[0])**2+(pt1[1]-pt2[1])**2)
-            r1 = np.arctan2(pt2[1]-pt1[1], pt2[0]-pt1[0]) - pt1[2]
+            ydiff = np.around(pt2[1]-pt1[1], 2)
+            xdiff = np.around(pt2[0]-pt1[0], 2)
+            r1 = np.arctan2(ydiff, xdiff) - pt1[2]
             r2 = pt2[2] - pt1[2] - r1
             godom = OdometryData(r1, tr, r2)
-            print(pt2)
 
+            print(godom)
+        if godom and gscan:
+            fs.fast_slam(godom, gscan)
+            godom = False
+            gscan = False
+
+        cv.waitKey(1)
+        
         read_first_odom = True
 
 def main():
@@ -84,6 +95,7 @@ def main():
     '''
     global gscan
     global godom
+    global fs
 
     rospy.init_node('slam', anonymous=True)
 
@@ -105,12 +117,12 @@ def main():
 
     rate = rospy.Rate(FREQ) # rate of the loop
     while not rospy.is_shutdown():
-        if godom and gscan:
-            fs.fast_slam(godom, gscan)
-            godom = False
-            gscan = False
+        # if godom and gscan:
+        #     fs.fast_slam(godom, gscan)
+        #     godom = False
+        #     gscan = False
 
-        cv.waitKey(1)
+        # cv.waitKey(1)
         rate.sleep()
 
 

scripts/odometry_data.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class OdometryData(object):
     """Represents an odometry command.
 
@@ -6,9 +8,21 @@ class OdometryData(object):
     - r2: final rotation in radians counterclockwise
     """
     def __init__(self, r1, tr, r2):
-        self.r1 = r1
+        self.r1 = self.normalize_angle(r1)
         self.tr = tr
-        self.r2 = r2
+        self.r2 = self.normalize_angle(r2)
+
+    def normalize_angle(self, angle):
+        """Normalize the angle between -pi and pi"""
+
+        while angle > np.pi:
+            angle = angle - 2 * np.pi
+
+        while angle < -np.pi:
+            angle = angle + 2 * np.pi
+
+        return angle
 
     def __str__(self):
-        return "Odometry(r1 = {0} rad, tr = {1} m, r2 = {2} rad)".format(self.r1, self.tr, self.r2)
+        # return "Odometry(r1 = {0} rad,\ntr = {1} m\n, r2 = {2} rad\n)".format(self.r1, self.tr, self.r2)
+        return "Odometry(r1 = {0} deg,\ntr = {1} m\n, r2 = {2} deg\n)".format(np.degrees(self.r1), self.tr, np.degrees(self.r2))

scripts/particle.py
@@ -17,7 +17,9 @@ class Particle(object):
         self.noise = noise
 
         # initialize robot pose at origin
-        self.pose = np.vstack([300, 300, 0])
+        # self.pose = np.vstack([300, 300, 0])
+        self.pose = np.vstack([0, 0, 0])
+
 
         # initialize weights uniformly
         self.weight = 1.0 / float(num_particles)
@@ -39,9 +41,9 @@ class Particle(object):
     def motion_update(self, odom):
         """Predict the new pose of the robot"""
 
-        odom.tr /= self.pixel_size
-        odom.r1 = np.degrees(odom.r1)
-        odom.r2 = np.degrees(odom.r2)
+        # odom.tr /= self.pixel_size
+        # odom.r1 = np.degrees(odom.r1)
+        # odom.r2 = np.degrees(odom.r2)
 
         # append the old position
         self.trajectory.append(self.pose)
@@ -57,13 +59,14 @@ class Particle(object):
 
         
         # Estimate of the new position of the Particle
-        x_new = self.pose[0] + translation_noisy * cos(self.pose[2] + delta_rot1_noisy)
-        y_new = self.pose[1] + translation_noisy * sin(self.pose[2] + delta_rot1_noisy)
-        theta_new = normalize_angle(self.pose[2] + delta_rot1_noisy + delta_rot2_noisy)
+        x_new = self.pose[0] + odom.tr * cos(self.pose[2] + odom.r1)
+        print(sin(self.pose[2] + odom.r1))
+        y_new = self.pose[1] + odom.tr * sin(self.pose[2] + odom.r1)
+        theta_new = normalize_angle(self.pose[2] + odom.r1 + odom.r2)
 
         self.pose = np.array([x_new, y_new, theta_new])
         
-        # print(int(self.pose[0]), int(self.pose[1]), int(self.pose[2]))
+        print(round(self.pose[0], 3), round(self.pose[1], 3), round(np.degrees(self.pose[2]), 3))
 
 
 
@@ -83,10 +86,10 @@ class Particle(object):
 def normalize_angle(angle):
     """Normalize the angle between -pi and pi"""
 
-    while angle > np.degrees(pi):
-        angle = angle - 2 * np.degrees(pi)
+    while angle > pi:
+        angle = angle - 2 * pi
 
-    while angle < np.degrees(-pi):
-        angle = angle + 2 * np.degrees(pi)
+    while angle < -pi:
+        angle = angle + 2 * pi
 
     return angle